0992ad5b82e563ad53bc2484490e22e1c198442b
danielgwilson <daniel@danielgwilson.com>
2016-11-25 23:59:40 +0200

    Finally got globals working, copied code for intents, added LUIS functionality and endpoint

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var restify = require('restify');
 var builder = require('botbuilder');
+var restify = require('restify');
 
 //=========================================================
 // Bot Setup
@@ -13,8 +13,8 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: process.env.appId,
-    appPassword: process.env.appPassword
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
@@ -23,6 +23,38 @@ server.post('/api/messages', connector.listen());
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Fuck");
-});
+var recognizer = new builder.LuisRecognizer('https://api.projectoxford.ai/luis/v2.0/apps/f0b89ad3-f79f-4aca-860a-09d4b77cd822?subscription-key=3ab22a9d89834a12a858adba547ab529&verbose=true');
+var intents = new builder.IntentDialog({ recognizers: [recognizer] });
+bot.dialog('/', intents);
+
+intents.matches(/^change name/i, [
+    function (session, args, next) {
+        session.beginDialog('/profile');
+    },
+    function (session, results) {
+        session.send('Ok... Changed your name to %s', session.userData.name);
+    }
+]);
+
+intents.onDefault([
+    function (session, args, next) {
+        if (!session.userData.name) {
+            session.beginDialog('/profile');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profile', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
