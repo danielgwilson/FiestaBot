a138701f2a1b905ab611282c23e7d1f6a59facc7
danielgwilson <daniel@danielgwilson.com>
2016-11-25 21:48:49 +0200

    Initial commit after following tutorial

---
+++ app.js
@@ -0,0 +1,28 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url);
+});
+
+// Create chat bot
+var connector = new builder.ChatConnector({
+    appId: 'f7bd09ea-ea4b-4c14-ade6-d2c6ce14f944',
+    appPassword: 'pSdhXX8GKhXA93uYmemHfDA'
+});
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
+bot.dialog('/', function (session) {
+    session.send("Fuck");
+});
